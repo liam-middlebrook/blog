2c6756ee8a2a9cefaebddbd98759acc972c79065
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-09-05 17:06:34 -0400

    Publishing: Publishing: Publishing: Updated/added missing posts

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 gitmsg=$(git log -1 --pretty=%B)
 echo ${gitmsg}
 git pull
-jekyll build -d ./
+jekyll build -d ./production
 git checkout production
+cp -R ./production/* ./
+rm -rf ./production
 git add *
 git commit -am "Publishing: ${gitmsg}"
 
