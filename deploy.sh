25028a120fd22ecf98be4d8e74eb58ad6d28896b
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-09-05 16:46:50 -0400

    Site Changes

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,8 @@
 gitmsg=$(git log -1 --pretty=%B)
 echo ${gitmsg}
 jekyll build -d production/
-cd production/
+git pull
 git add *
 git commit -am "Publishing: ${gitmsg}"
 git push
-cd ..
-git commit -am "Updated production submodule"
-git push
+
