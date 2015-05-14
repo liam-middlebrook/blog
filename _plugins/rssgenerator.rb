a180bc51f4ccdb16fbc8a81e2b62687f6c3d2a34
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-05-14 00:57:55 -0400

    strippin html

--- _plugins/rssgenerator.rb
+++ _plugins/rssgenerator.rb
@@ -56,7 +56,7 @@ module Jekyll
             item.link = link
             item.description = ''
             item.updated = post.date
-            item.summary = post.content | strip_html
+            item.summary = "<![CDATA[" + post.excerpt.gsub(%r{</?[^>]+?>}, '') + "]]>"
           end
         end
       end
